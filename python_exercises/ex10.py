1552de948c9f631d31ed87b7a44980b56f3725c4
theintthandarnaing <theintthandarnaing123@gmail.com>
2018-06-25 14:43:57 +0530

    ex10.py

---
+++ python_exercises/ex10.py
@@ -0,0 +1,19 @@
+tabby_cat="\tI'm tabbed in";
+persian_cat="I'm split\non s line."
+backslash_cat="i'm \\ a \\ cat"
+fat_cat="I'll do a list:\t* Cat food \t* Fishies \t* Catnip\n\t* Grass"
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+
+
+
+
+
+
+
+
+
+
+
